6356f8441c3e392e84f9e5636155706762f5c2b5
mykdolnyk <mdolnyk@gmail.com>
2025-07-28 10:37:12 +0300

    Changed the MySQL DB to the PostgreSQL one

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo 'Waiting for the DB to start...'
-until nc -z "$MYSQL_HOST" "$MYSQL_PORT"; do
-  sleep 1
-done
+# echo 'Waiting for the DB to start...'
+# until nc -z "$MYSQL_HOST" "$MYSQL_PORT"; do
+#   sleep 1
+# done
 
 echo "Collecting Static..."
 python manage.py collectstatic --noinput
